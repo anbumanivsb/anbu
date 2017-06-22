6a01f1fcd2d79df3e75454e44d2349fbd1da8c8f
anbumanivsb <anbuanc@gmail.com>
2017-06-22 13:07:32 +0530

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main(){
-    printf("Hello World");
-    printf("Hello World");
-    printf("Hello World");
-    printf("Hello World");
-    printf("Hello World");
+    printf("Hello");
+    printf("Hello");
+    printf("Hello");
+    printf("Hello");
+    printf("Hello");
     return 0;
 }
